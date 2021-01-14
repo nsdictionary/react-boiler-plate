49a18ca1a1acbc6ecf53d6c999c03df1c09d6ab6
sam <3161728@gmail.com>
2021-01-14 23:08:34 +0900

    Initialize boiler plate project

---
+++ index.ts
@@ -0,0 +1,23 @@
+import * as express from "express";
+import * as Mongoose from "mongoose";
+require("dotenv").config();
+
+const app = express();
+const port = 5000;
+
+Mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+})
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
