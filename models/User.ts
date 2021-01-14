8b7eda40f3f4111313ff0d987e1778d38b32f918
sam <3161728@gmail.com>
2021-01-15 00:49:06 +0900

    Encryption password

--- models/User.ts
+++ models/User.ts
@@ -1,6 +1,23 @@
-import { Schema, model } from "mongoose";
+import { Schema, model, Model, Document } from "mongoose";
+import * as bcrypt from "bcrypt";
+import { NextFunction } from "express";
 
-const userSchema = new Schema({
+export interface IUser {
+  email: string;
+  password: string;
+  name?: string;
+  lastname?: string;
+  role: number;
+  image?: string;
+  token?: string;
+  tokenExp?: number;
+}
+
+export interface IUserModel extends IUser, Document {
+  fullName(): string;
+}
+
+const userSchema: Schema<IUserModel> = new Schema({
   name: {
     type: String,
     maxlength: 50,
@@ -31,5 +48,24 @@ const userSchema = new Schema({
   },
 });
 
-const User = model("User", userSchema);
+userSchema.pre<IUserModel>("save", function (next: NextFunction) {
+  const user = this;
+  if (user.isModified("password")) {
+    const saltRounds = 10;
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) {
+        return next(err);
+      }
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) {
+          return next(err);
+        }
+        user.password = hash;
+        next();
+      });
+    });
+  }
+});
+
+const User: Model<IUserModel> = model<IUserModel>("User", userSchema);
 export default User;
