87b0f2147cf11ed23d1d2c89337b4209f1b2f41c
sam <3161728@gmail.com>
2021-01-16 01:04:47 +0900

    Divide user router

--- server/index.ts
+++ server/index.ts
@@ -2,16 +2,17 @@ import * as express from "express";
 import * as Mongoose from "mongoose";
 import * as BodyParser from "body-parser";
 import * as CookieParser from "cookie-parser";
+import * as cors from "cors";
+import UserRouter from "./routes/users";
 import config from "./config/key";
-import User from "./models/User";
-import auth from "./middleware/auth";
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(BodyParser.urlencoded({ extended: true }));
 app.use(BodyParser.json());
 app.use(CookieParser());
+app.use(cors());
 
 Mongoose.connect(config.mongoURI, {
   useNewUrlParser: true,
@@ -22,76 +23,8 @@ Mongoose.connect(config.mongoURI, {
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/api/hello", (req, res) => {
-  res.send("Hello!!~~~");
-});
-
-app.post("/api/users/register", async (req, res) => {
-  const user = new User(req.body);
-  await user.save((err, userInfo) => {
-    if (err) {
-      return res.json({ ok: false, err });
-    }
-    return res.status(200).json({ ok: true });
-  });
-});
-
-app.post("/api/users/login", (req, res) => {
-  User.findOne({ email: req.body.email }, (err, user) => {
-    if (!user) {
-      return res.json({ ok: false, err: "User not found" });
-    }
-
-    user.comparePassword(req.body.password, (err, isMatch) => {
-      if (!isMatch) {
-        return res.json({ ok: false, err: "password not matched" });
-      }
-
-      user.generateToken((err, user) => {
-        if (err) {
-          return res.status(400).send(err);
-        }
-
-        res
-          .cookie("x_auth", user.token)
-          .status(200)
-          .json({ ok: true, userId: user._id });
-      });
-    });
-  });
-});
-
-app.get("/api/users/auth", auth, (req: any, res) => {
-  res.status(200).json({
-    ok: true,
-    userId: req.user._id,
-    isAdmin: req.user.role === 0 ? false : true,
-    isAuth: true,
-    email: req.user.email,
-    lastname: req.user.lastname,
-    role: req.user.role,
-    image: req.user.image,
-  });
-});
-
-app.get("/api/users/logout", auth, (req: any, res) => {
-  User.findOneAndUpdate(
-    { _id: req.user._id },
-    { token: "" },
-    {},
-    (err, user) => {
-      if (err) {
-        return res.json({ ok: false, err });
-      }
-      return res.status(200).send({ ok: true });
-    }
-  );
-});
+app.use("/api/users", UserRouter);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server Listening on ${port}`);
 });
---
+++ server/routes/users.ts
@@ -0,0 +1,69 @@
+import * as express from "express";
+import User from "../models/User";
+import auth from "../middleware/auth";
+
+const router = express.Router();
+
+router.post("/api/users/register", async (req, res) => {
+  const user = new User(req.body);
+  await user.save((err, userInfo) => {
+    if (err) {
+      return res.json({ ok: false, err });
+    }
+    return res.status(200).json({ ok: true });
+  });
+});
+
+router.post("/api/users/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({ ok: false, err: "User not found" });
+    }
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({ ok: false, err: "password not matched" });
+      }
+
+      user.generateToken((err, user) => {
+        if (err) {
+          return res.status(400).send(err);
+        }
+
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ ok: true, userId: user._id });
+      });
+    });
+  });
+});
+
+router.get("/api/users/auth", auth, (req: any, res) => {
+  res.status(200).json({
+    ok: true,
+    userId: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
+router.get("/api/users/logout", auth, (req: any, res) => {
+  User.findOneAndUpdate(
+    { _id: req.user._id },
+    { token: "" },
+    {},
+    (err, user) => {
+      if (err) {
+        return res.json({ ok: false, err });
+      }
+      return res.status(200).send({ ok: true });
+    }
+  );
+});
+
+export default router;
