865159a8c67dc54fc50f86c1cb77ccb44fe1ef1a
sam <3161728@gmail.com>
2021-01-15 13:15:43 +0900

    Install react router dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
 
 function App() {
-  return <div className="App">Hello React!!</div>;
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/register" component={RegisterPage} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
