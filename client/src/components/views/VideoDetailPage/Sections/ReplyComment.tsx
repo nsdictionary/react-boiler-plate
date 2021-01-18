bc005c057d9dc6a9fb65ad68b041b929a795611d
sam <3161728@gmail.com>
2021-01-18 13:56:53 +0900

    Implement replay comment compnent

--- client/src/components/views/VideoDetailPage/Sections/ReplyComment.tsx
+++ client/src/components/views/VideoDetailPage/Sections/ReplyComment.tsx
@@ -1,7 +1,59 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import SingleComment from "./SingleComment";
 
 const ReplyComment = (props: any) => {
-  return <div>ReplyComment</div>;
+  const [ChildCommentNumber, setChildCommentNumber] = useState<number>(0)
+  const [OpenReplyComments, setOpenReplyComments] = useState<boolean>(false)
+
+  useEffect(() => {
+    let commentNumber = 0;
+    props.CommentLists.map((comment: any) => {
+      if (comment.responseTo === props.parentCommentId) {
+        commentNumber++
+      }
+    })
+    setChildCommentNumber(commentNumber)
+  }, [props.CommentLists, props.parentCommentId])
+
+  const renderReplyComment = (parentCommentId: string) =>
+    props.CommentLists.map((comment: any, index: number) => (
+      <React.Fragment key={index}>
+        {comment.responseTo === parentCommentId &&
+        <div style={{width: '80%', marginLeft: '40px'}}>
+          <SingleComment
+              comment={comment}
+              videoId={props.videoId}
+              refreshFunction={props.refreshFunction}
+          />
+          <ReplyComment
+              CommentLists={props.CommentLists}
+              parentCommentId={comment._id}
+              videoId={props.videoId}
+              refreshFunction={props.refreshFunction}
+          />
+        </div>
+        }
+      </React.Fragment>
+    ))
+
+  const handleChange = () => {
+    setOpenReplyComments(!OpenReplyComments)
+  }
+
+  return (
+    <div>
+      {ChildCommentNumber > 0 &&
+        <p
+          style={{fontSize: '14px', margin: 0, color: 'gray'}}
+          onClick={handleChange}
+        >
+          View {ChildCommentNumber} more comment(s)
+        </p>
+      }
+
+      {OpenReplyComments && renderReplyComment(props.parentCommentId)}
+    </div>
+  )
 };
 
 export default ReplyComment;
